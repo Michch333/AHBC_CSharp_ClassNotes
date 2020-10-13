23111492b835e9c1eb36f3113ce7d02f7afb8da1
Mike C <Michch333@gmail.com>
2020-10-12 21:36:26 -0400

    first commit for notes

---
+++ FirstDayOfClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Dynamic;
+
+namespace FirstDayOfClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double doubleToInt = 22.9;
+            int castedInt = (int)doubleToInt;
+
+            Console.WriteLine(castedInt);
+
+            bool canDrink = castedInt == 21;
+            Console.WriteLine(canDrink);
+
+            int mikeAge = 26;
+
+            bool mikeCanDrink = mikeAge > 21;
+            bool mikeIsUnderAge = !mikeCanDrink;
+
+            Console.WriteLine($"Mike can drink is {mikeCanDrink}, which would make mike is underage to: {mikeIsUnderAge}");
+            Console.ReadLine();
+
+        }
+    }
+}
